5e45f6f563e8af715ffee45e1d56fd6df0275dd6
Abdul Hannan Kanji <akanji@swarm2.cm.cluster>
2020-10-10 14:35:52 -0400

    Add intermediate results

--- runexp.sh
+++ runexp.sh
@@ -7,16 +7,16 @@
 
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-11:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=200    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=400mb    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
 
 export MKL_NUM_THREADS=7
 export OPENBLAS_NUM_THREADS=7
 export OMP_NUM_THREADS=7
-/home/akanji/miniconda3/envs/seldnian-pre/bin/python  experiment_synthetic.py
+/home/akanji/miniconda3/envs/seldnian-pre/bin/python  experiment_synthetic.py $1
 
 sleep 1
