a47d2eaebee6e1f4eae4f3f4e5c13e9141f5250f
Abdul Hannan Kanji <akanji@swarm2.cm.cluster>
2020-10-11 14:42:41 -0400

    send all args to script

--- runexp.sh
+++ runexp.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-11:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=400mb    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=300mb    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
 
--- runexp_p.sh
+++ runexp_p.sh
@@ -7,16 +7,16 @@
 
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=0-11:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=400mb    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=10gb    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
 
 export MKL_NUM_THREADS=7
 export OPENBLAS_NUM_THREADS=7
 export OMP_NUM_THREADS=7
-/home/akanji/miniconda3/envs/seldnian-pre/bin/python  experiment_s_p.py $1
+/home/akanji/miniconda3/envs/seldnian-pre/bin/python  experiment_s_p.py "$@"
 
 sleep 1
