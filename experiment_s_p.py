011dd301f9a21451cef0a73e998a04832ee4c68b
Abdul Hannan Kanji <akanji@umass.edu>
2020-10-12 01:51:18 -0400

    Add config and dynamic save

--- experiment_s_p.py
+++ experiment_s_p.py
@@ -26,7 +26,7 @@ args = parser.parse_args()
 if args.dir:
     dir = args.dir
 else:
-    dir = 'results_8'
+    dir = 'results_default'
 
 if args.config:
     exp_config = json.load(open(args.config, "r"))
@@ -115,6 +115,7 @@ def run_experiment_p(exp):
         uc_failure_rate.append(1 if ghat_val > 0.0 else 0)
 
         results.update({
+            'n': n,
             'trials': t,
             'failure_rate': np.mean(failure_rate),
             'failure_rate_std': np.std(failure_rate),
@@ -133,80 +134,22 @@ def run_experiment_p(exp):
     return results
 
 
-def run_experiment(exp):
-    for n in exp['N']:
-        X, y, A_idx = make_synthetic(n, exp['D'], *exp['tprs'])
-        X_test, y_test, _ = make_synthetic(n * 100, exp['D'], *exp['tprs'], A_idx=A_idx)
-        for opt in exp['opt']:
-            if opt not in results:
-                results[opt] = []
-            failure_rate = []
-            accuracy = []
-            mean_ghat = []
-            uc_failure_rate = []
-            uc_accuracy = []
-            uc_mean_ghat = []
-            for t in np.arange(exp['trials']):
-                ghats = []
-                ghats.append({
-                    'fn': ghat_tpr_diff(A_idx,
-                                        threshold=abs(exp['tprs'][0] - exp['tprs'][1]) / 2),
-                    'delta': 0.05
-                })
-                if opt == 'CMAES':
-                    est = SeldonianAlgorithmLogRegCMAES(X, y, test_size=exp['test_size'],
-                                                        g_hats=ghats,
-                                                        verbose=True)
-                else:
-                    est = LogisticRegressionSeldonianModel(X, y, test_size=exp['test_size'],
-                                                           g_hats=ghats,
-                                                           verbose=True)
-                est.fit()
-
-                acc = accuracy_score(y, est.predict(X))
-                safe = est.safetyTest(predict=False)
-                mean_ghat.append(safe)
-                failure_rate.append(1 if safe > 0.0 else 0)
-                accuracy.append(acc)
-
-                uc_est = LogisticRegression(penalty='none').fit(X, y)
-                uc_acc = accuracy_score(y, uc_est.predict(X))
-                y_preds = uc_est.predict(X)
-                ghat_val = ghats[0]['fn'](X, y, y_preds, ghats[0]['delta'], ub=False)
-                uc_failure_rate.append(1 if ghat_val > 0.0 else 0)
-                uc_accuracy.append(uc_acc)
-                uc_mean_ghat.append(ghat_val)
-            results[opt].append({
-                'failure_rate': np.mean(failure_rate),
-                'failure_rate_std': np.std(failure_rate),
-                'accuracy': np.mean(accuracy),
-                'ghat': np.mean(mean_ghat),
-                'uc_failure_rate': np.mean(uc_failure_rate),
-                'uc_failure_rate_std': np.std(uc_failure_rate),
-                'uc_accuracy': np.mean(uc_accuracy),
-                'uc_ghat': np.mean(uc_mean_ghat)
-            })
-            save_res(results)
-            print(f"Result for config: N = {n}, opt= {opt}")
-            print(f"{results[opt][-1]!r}")
-
-
 if __name__ == '__main__':
     print(f"Running experiment with checkpoint: {checkpoint}")
     print(f"Config used: {exp_config!r}")
+    if 'name' in exp_config:
+        dir = f"result_{exp_config['name']}"
     os.makedirs(dir, exist_ok=True)
 
-    nWorkers = 16
-    if args.threads:
-        nWorkers = args.threads
-
-    print(f"Running experiments on {nWorkers} threads")
     ray.init()
+    pickle.dump(exp_config, open(dir + "/config.p", "wb"))
+
     exps = []
     for n in exp_config['N']:
         exps.append(copy.deepcopy(exp_config))
         exps[-1]['N'] = n
 
+    pickle.dump(exps, open(f"{dir}/exps.p", "wb"))
     a = time()
     futures = [run_experiment_p.remote(x) for x in exps]
     res = ray.get(futures)
