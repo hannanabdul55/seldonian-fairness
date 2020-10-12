88c05c4ce9913772f197bd7647ba274d0b639231
Abdul Hannan Kanji <akanji@swarm2.cm.cluster>
2020-10-12 10:05:31 -0400

    clean files + add results

--- runexp_p.sh
+++ runexp_p.sh
@@ -3,14 +3,14 @@
 #SBATCH --job-name=exp_run
 #SBATCH --output=res_%j.txt  # output file
 #SBATCH -e res%j.err        # File to which STDERR will be written
-#SBATCH --partition=defq    # Partition to submit to
+#SBATCH --partition=longq    # Partition to submit to
 
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0-11:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=10gb    # Memory in MB per cpu allocated
+#SBATCH --time=0-22:59         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=4gb    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
 
---
+++ runexp_p_powell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --job-name=exp_run_pow
+#SBATCH --output=res_%j.txt  # output file
+#SBATCH -e res%j.err        # File to which STDERR will be written
+#SBATCH --partition=longq    # Partition to submit to
+
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-22:59         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=4gb    # Memory in MB per cpu allocated
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=akanji@cs.umass.edu
+
+export MKL_NUM_THREADS=7
+export OPENBLAS_NUM_THREADS=7
+export OMP_NUM_THREADS=7
+/home/akanji/miniconda3/envs/seldnian-pre/bin/python  experiment_s_p.py "$@"
+
+sleep 1
