356558168ce03d7935ba4fc93c2da48d4dd89475
Abdul Hannan Kanji <akanji@umass.edu>
2020-11-16 15:23:24 -0500

    fix pytorch lightning import

--- seldonian/seldonian_nn.py
+++ seldonian/seldonian_nn.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from torch.utils.data import random_split, DataLoader
 
-import pytorch_lightning as pl
 import numpy as np
 
 from seldonian.algorithm import SeldonianAlgorithm
@@ -73,12 +72,12 @@ class VanillaNN(SeldonianAlgorithm):
             self.lagrange = None
 
         self.dataset = torch.utils.data.TensorDataset(self.X, self.y)
-        self.loader = DataLoader(self.dataset, batch_size=300)
+        self.loader = DataLoader(self.dataset, batch_size=1000)
         if self.lagrange is not None:
             params = nn.ParameterList(self.mod.parameters())
-            self.optimizer = torch.optim.Adam(params, lr=3e-4)
+            self.optimizer = torch.optim.Adam(params, lr=6e-4)
             # self.l_optimizer = torch.optim.Adam([self.lagrange], lr=0.1)
-            self.l_optimizer = torch.optim.Adam([self.lagrange], lr=3e-2)
+            self.l_optimizer = torch.optim.Adam([self.lagrange], lr=5e-3)
         else:
             self.optimizer = torch.optim.Adam(self.mod.parameters(), lr=3e-3)
             self.l_optimizer = None
@@ -154,89 +153,6 @@ class VanillaNN(SeldonianAlgorithm):
     def data(self):
         return self.X, self.y
 
-
-class NeuralNetModel(SeldonianAlgorithm, pl.LightningModule):
-    def __init__(self, X, y, test_size=0.4, g_hats=[], verbose=False, hard_barrier=False,
-                 stratify=False):
-        super().__init__()
-        self.X = X
-        self.y = y
-        D = self.X.shape[1]
-        H1 = int(D * 1.6)
-        self.constraint = g_hats
-        self.X, self.X_s, self.y, self.y_s = train_test_split(
-            self.X, self.y, test_size=test_size, random_state=0,
-            stratify=[0, 1] if stratify else None
-        )
-        self.verbose = verbose
-        self.mod = nn.Sequential(
-            nn.Linear(D, H1),
-            nn.ReLU(),
-            nn.Linear(H1, 2)
-        )
-        self.loss_fn = nn.CrossEntropyLoss()
-        if len(self.constraint) > 0:
-            self.lagrange = torch.rand((len(self.constraint),), requires_grad=True)
-        else:
-            self.lagrange = None
-
-        self.dataset = torch.utils.data.TensorDataset(torch.tensor(X), torch.tensor(y))
-        self.loader = DataLoader(self.dataset)
-
-    def forward(self, x):
-        return self.mod(x)
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self.mod(x)
-        safety = self.safetyTest(predict=True)
-        loss = self.loss_fn(y_pred, y)
-        loss += self.lagrange.dot(safety)
-        return loss
-
-    def configure_optimizers(
-            self,
-    ):
-        print(f"Lagrange multipliers: {self.lagrange}")
-        # print(f"Lagrange multipliers: {list(self.parameters())}")
-        if self.lagrange is not None:
-            optimizer = torch.optim.Adam(list(self.parameters()) + list(self.lagrange),
-                                         lr=3e-3)
-        else:
-            optimizer = torch.optim.Adam(self.parameters(), lr=3e-3)
-        return optimizer
-
-    def safetyTest(self, predict=False, ub=True):
-        print("In safety test")
-        X_test = self.X if predict else self.X_s
-        y_test = self.y if predict else self.y_s
-
-        ghats = torch.zeros(len(self.constraint), requires_grad=True)
-        i = 0
-        for g_hat in self.constraints:
-            y_preds = self.forward(X_test)
-            ghat_val = g_hat['fn'](X_test, y_test, y_preds, g_hat['delta'], self.X_s.shape[0],
-                                   predict=predict, ub=ub)
-            # if ghat_val > 0:
-            #     if self.hard_barrier is True and predict is True:
-            #         return 1
-            #     else:
-            #         return ghat_val
-            ghats[i] += ghat_val
-            i += 1
-        return ghats
-        pass
-
-    def data(self):
-        return self.X, self.y
-
-    def fit(self, **kwargs):
-        trainer = pl.Trainer()
-        trainer.fit(self, self.loader)
-        pass
-
-    def predict(self, X):
-        return self.__call__(X)
 
 
 def grad_check(named_params):
