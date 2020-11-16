f2d7eaf9b42e08af6751b6b1c721afa89bcd4863
Abdul Hannan Kanji <akanji@umass.edu>
2020-11-16 17:42:13 -0500

    update gpus

--- runexp_p_gypsum.sh
+++ runexp_p_gypsum.sh
@@ -7,7 +7,7 @@
 
 #
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2-12:59         # Maximum runtime in D-HH:MM
