c3958e67f5484637a458c601e4f6a5268fed08ed
Abdul Hannan Kanji <akanji@umass.edu>
2020-11-18 13:44:29 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from os import path
 
-req = open("requirements.txt", 'r').read()
+req = open("requirements-release.txt", 'r').read()
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -9,13 +9,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='seldonian',
-    version='0.0.1dev',
+    version='0.0.2dev',
     packages=['seldonian'],
     install_requires=req.splitlines(),
+    python_requires='>3.5',
     url='https://abdulhannan.in/seldonian-fairness',
     license='MIT',
     author='Abdul Hannan Kanji',
-    author_email='hannanabdul55@gmail.com',
+    author_email='kanji@adulhannan.in',
     description='Train model with safety constraints',
     long_description=long_description,
     long_description_content_type='text/markdown'
