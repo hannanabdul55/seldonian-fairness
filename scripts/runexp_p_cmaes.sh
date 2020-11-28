b62e90d7fd476355725d671a9e19d2aa57991591
Abdul Hannan Kanji <akanji@swarm2.cm.cluster>
2020-11-28 18:26:19 -0500

    add final week results

--- scripts/runexp_p_cmaes.sh
+++ scripts/runexp_p_cmaes.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2-12:59         # Maximum runtime in D-HH:MM
-#SBATCH --mem-per-cpu=4gb    # Memory in MB per cpu allocated
+#SBATCH --mem-per-cpu=6gb    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
 export PYTHONPATH=/home/akanji/copy_seldonian/seldonian-fairness:$PYTHONPATH
