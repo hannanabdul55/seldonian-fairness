b0bd851120a64325d78000b7b55d558c989322c0
Abdul Hannan <abdul.kanji@sway-ai.com>
2021-08-07 13:25:27 -0400

    Fix safety test error

--- seldonian/seldonian.py
+++ seldonian/seldonian.py
@@ -343,7 +343,7 @@ class LogisticRegressionSeldonianModel(SeldonianAlgorithm):
                     self.X, self.y, test_size=test_size, random_state=random_seed
                 )
             else:
-                thets = [np.random.default_rng(random_seed + i).random((X.shape[1] + 1, 1)) for i
+                thets = [np.random.default_rng(random_seed + i).random((X.shape[1] + 1,)) for i
                          in
                          range(nthetas)]
                 best_diff = np.inf * (1 if agg_fn == 'min' else -1)
