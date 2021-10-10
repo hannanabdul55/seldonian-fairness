0e235c9263b8c364a04cf96e76d053edad273578
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-10-10 12:09:40 -0400

    updtae script

--- run_cma_exp.sh
+++ run_cma_exp.sh
@@ -3,17 +3,17 @@
 #SBATCH --job-name=exp_cma_run
 #SBATCH --output=logs/res_%j.txt  # output file
 #SBATCH -e logs/res%j.err        # File to which STDERR will be written
-#SBATCH --partition=defq    # Partition to submit to
+#SBATCH --partition=longq    # Partition to submit to
 
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0-22:59         # Maximum runtime in D-HH:MM
+#SBATCH --time=1-22:59         # Maximum runtime in D-HH:MM
 #SBATCH --mem-per-cpu=500    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
-#SBATCH --array=1-100
+#SBATCH --array=1-1000%100
 export PYTHONPATH=/home/akanji/seldonian-fairness:$PYTHONPATH
 export MKL_NUM_THREADS=7
 export OPENBLAS_NUM_THREADS=7
