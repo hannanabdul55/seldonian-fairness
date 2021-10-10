970b6f0adda577009626fa48a9ac7b668b3a67c1
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2021-10-10 12:08:40 -0400

    fix number of jobs

--- run_cma_exp.sh
+++ run_cma_exp.sh
@@ -13,7 +13,7 @@
 #SBATCH --mem-per-cpu=500    # Memory in MB per cpu allocated
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akanji@cs.umass.edu
-#SBATCH --array=1-1000%100
+#SBATCH --array=1-100
 export PYTHONPATH=/home/akanji/seldonian-fairness:$PYTHONPATH
 export MKL_NUM_THREADS=7
 export OPENBLAS_NUM_THREADS=7
